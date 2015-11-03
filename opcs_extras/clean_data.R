23e084be0acadad993cc60f0aa2c3f5589a27269
sdevine188 <sdevine188@gmail.com>
2015-11-03 17:36:13 -0500

    updated

--- opcs_extras/clean_data.R
+++ opcs_extras/clean_data.R
@@ -19,26 +19,41 @@ md$address <- str_c(md$Appl.Street.Addr.1, md$Appl.City.Name, md$Appl.State.Abbr
 md$address <- as.character(md$address)
 
 # split into 2500 row sizes to stay below goolge maps api daily limit
-md1 <- md[1:3, ]
+md0 <- md[1:3, ]
 md1 <- md[1:2500, ]
+date1 <- as.character(Sys.Date())
+date2 <- str_replace_all(date1, "-", "")
+md1_filename <- str_c("md1_", date2, ".csv")
+write.csv(md1, file = md1_filename, row.names = FALSE)
+
 md2 <- md[2501:5000, ]
+date1 <- as.character(Sys.Date())
+date2 <- str_replace_all(date1, "-", "")
+md2_filename <- str_c("md2_", date2, ".csv")
+write.csv(md2, file = md2_filename, row.names = FALSE)
+
+md4 <- md[7501:10000, ]
+date1 <- as.character(Sys.Date())
+date2 <- str_replace_all(date1, "-", "")
+md4_filename <- str_c("md4_", date2, ".csv")
+write.csv(md4, file = md4_filename, row.names = FALSE)
 
 # geocode addresses
-for(i in 1:nrow(md2)){
-        coordinates <- tryCatch(geocode(md2$address[i], service = "bing"), 
+for(i in 1:nrow(md4)){
+        coordinates <- tryCatch(geocode(md4$address[i], service = "bing"), 
                                 warning = function(w) {
                                         if(grepl("geocode failed with status ZERO_RESULTS", w)){
                                                 print("this is a warning")
-                                                address <- md2$address[i]
-                                                zip <- md2$Appl..Zip[i]
+                                                address <- md4$address[i]
+                                                zip <- md4$Appl..Zip[i]
                                                 geocode(zip, service = "bing")
                                         }
                                 },
                                 error = function(e) {
                                         if(grepl("replacement has length zero", e)){
                                                 print("this is an error")
-                                                address <- md2$address[i]
-                                                zip <- md2$Appl..Zip[i]
+                                                address <- md4$address[i]
+                                                zip <- md4$Appl..Zip[i]
                                                 geocode(zip, service = "bing")
                                         }
                                 } 
@@ -48,19 +63,19 @@ for(i in 1:nrow(md2)){
         print(coordinates)
         if(is.null(coordinates)){
                 print("error: coordinates are null")
-                address <- md2$address[i]
-                zip <- md2$Appl..Zip[i]
+                address <- md4$address[i]
+                zip <- md4$Appl..Zip[i]
                 coordinates <- geocode(zip, service = "bing")
                 coordinates <- unlist(coordinates)
                 print(coordinates)
-                md2$lon[i] <- coordinates[1]
-                md2$lat[i] <- coordinates[2]
+                md4$lon[i] <- coordinates[1]
+                md4$lat[i] <- coordinates[2]
         }
-        md2$lon[i] <- coordinates[1]
-        md2$lat[i] <- coordinates[2]
+        md4$lon[i] <- coordinates[1]
+        md4$lat[i] <- coordinates[2]
 }
 
-# which(is.na(md1$lat))
+which(is.na(md2$lat))
 # which(md1$Appl..Zip == "") 
 
 df <- data.frame(address = c("56 talmadge hill road, prospect, ct 06712", "22 skyline drive, prospect, ct 06712", 
@@ -113,7 +128,7 @@ for(i in 1:nrow(df)){
 
 # Error in md1$lon[i] <- coordinates[1] : replacement has length zero
 
-
+install.packages("taRifx.geo")
 options(BingMapsKey = "QFG8iBCiInAj6ER1ubuD~I5piVwPPZghOvhCJzBP-1g~AicfV1u7mkoKlY53KfatxR67u-NHXCfu1iEA8dBryA8vlUJy3yu3y0u2cZLWf-D4")
 
 gc <- geocode("Post Office Box 333, Toronto, SD, 57268", service = "bing")
